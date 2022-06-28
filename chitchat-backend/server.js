c1c353af4eccea91268b4a0fb0920d634b8e418e
tarunyadav148 <tarunydav148@gmail.com>
2022-06-28 17:48:48 +0530

    cors origin updated

--- chitchat-backend/server.js
+++ chitchat-backend/server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = process.env.PORT || 3000;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://chitchat148.netlify.app/',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 })
